95b78b615055581cc1bb91bb558c1568896d2ae6
siyic <89478754@qq.com>
2022-04-16 21:58:15 +0800

    v1.0.1_220416

---
+++ config/defaultSettings.ts
@@ -0,0 +1,22 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-layout';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  // 拂晓蓝
+  primaryColor: '#1890ff',
+  layout: 'mix',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '智慧停车平台',    //'Ant Design Pro',
+  pwa: false,
+  // logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
+  logo: '/logo.svg',
+  iconfontUrl: '',
+};
+
+export default Settings;
